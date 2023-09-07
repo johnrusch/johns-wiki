e3c0f2831cf0266c4cf493d992251c05ff2d476a
John Rusch <35750236+johnrusch@users.noreply.github.com>
2023-09-07 16:58:20 -0700

    changed border radius of nav icon

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,18 @@
 // 1. Import utilities from `astro:content`
-import { defineCollection } from "astro:content";
+import { defineCollection, z } from "astro:content";
 // 2. Define your collection(s)
 const blogCollection = defineCollection({
+  type: "content",
+  schema: ({ image }) => z.object({
+    title: z.string(),
+    pubDate: z.date(),
+    updated: z.date().optional(),
+    description: z.string(),
+    image: image().optional(),
+    imageAlt: z.string().optional(),
+    tags: z.array(z.string()),
+    draft: z.boolean(),
+  }),
   /* ... */
 });
 // 3. Export a single `collections` object to register your collection(s)
