7f114094d24c16c75394c3ad6c06e143c531676d
John Rusch <35750236+johnrusch@users.noreply.github.com>
2023-10-03 13:51:59 -0700

    put border images in public folder

--- src/scripts/borderImage.js
+++ src/scripts/borderImage.js
@@ -4,17 +4,17 @@ const getRandomBox = (arr) => {
   return arr[randomIndex];
 };
 const boxes = [
-  "../images/borderBox1.webp",
-  "../images/borderBox2.webp",
-  "../images/borderBox3.webp",
-  "../images/borderBox4.webp",
+  "/borderBox1.webp",
+  "/borderBox2.webp",
+  "/borderBox3.webp",
+  "/borderBox4.webp",
 ];
 
 const listCards = document.querySelectorAll('.list-card');
 listCards.forEach(card => {
   card.style.border = '.7rem solid transparent';
   card.style.borderImage = `url(${getRandomBox(boxes)}) 20 fill`;
-  card.style.WebkitBorderImage = "url(../images/borderBox1.webp) 85 fill";
+  card.style.WebkitBorderImage = `url(${getRandomBox(boxes)}) 85 fill`;
   card.style.OBorderImage = `url(${getRandomBox(boxes)}) 20 fill`;
   card.style.borderImageSlice = '90 fill';
 });
