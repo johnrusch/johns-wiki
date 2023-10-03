7913934818f6cf41741a253f01d460ce919f8dec
John Rusch <35750236+johnrusch@users.noreply.github.com>
2023-10-03 07:57:00 -0700

    removed image service from vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,8 +7,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   output: "static",
-  adapter: vercelStatic({
-    imageService: true,
-  }),
+  adapter: vercelStatic(),
   integrations: [react(), tailwind()]
 });
